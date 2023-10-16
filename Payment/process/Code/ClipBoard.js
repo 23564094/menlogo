6fa509095bf809edc24fc20f61bfca7f72d1860a
23564094 <138080901+23564094@users.noreply.github.com>
2023-10-17 00:55:57 +0600

    Update ClipBoard.js

--- Payment/process/Code/ClipBoard.js
+++ Payment/process/Code/ClipBoard.js
@@ -1,27 +1,50 @@
 $(document).ready(function() {
-  $('button').click(function(){
-    var btntxt = $(this).text();
+  
+/*  ANIMATION COLORS  */
+  $("div[class*=primary], .color__btn-1, .color__btn-1>i").on("click", function(e) {
+    e.preventDefault;
+    // move just if click the background
+    if (event.target !== this)
+    return;
+    $("div[class*=primary], .color__btn-1").toggleClass("move");
     
-     var copy = $(this).parent().find('.copy').text();
-     
-     var $temp = $("<input>");
-     $("body").append($temp);
-     $temp.val(copy).select();
-     document.execCommand("copy");
-     $temp.remove();
-    
-    
-    $('.confirmation').hide().html('Copied <b>' + btntxt + ' code</b> to clipboard').fadeIn(100).delay(800).fadeOut(200);
-   
-    $( '.main' ).trigger( "click" );
+    $(".color__box-2").toggleClass("move");
   });
   
-  $('.main div').click(function(){
-      var range = document.createRange();
-      var selection = window.getSelection();
-      range.selectNodeContents(this);
-      selection.removeAllRanges();
-      selection.addRange(range);
+  $("div[class*=secondary], .color__btn-2, .color__btn-2>i").on("click", function() {
+    
+    // move just if click the background
+    if (event.target !== this)
+    return;
+    $("div[class*=secondary], .color__btn-2").toggleClass("move");
+  });
+
+/*  COPY TO CLIPBOARD  */
+
+// click the icon
+$("[id^=copy-]").on("click", function(e) {
+  e.preventDefault;
+  
+  
+  // find the id number of the parent
+  let num = $(this).parent().attr("id").slice("color-".length);
+  let elem = $(".color-" + num);
+  
+  // copy the text of the background color
+  let $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val(elem.children(":first").text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  
+  $(this).children(".tooltip").addClass("appear").delay(1000).queue(function( next ){
+    $(this).removeClass("appear"); 
+    $(this).dequeue();
   });
 });
+
+});
+
+
+
 
