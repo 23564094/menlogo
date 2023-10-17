557679e9a25ac3f84d81e857d7c021f66e7236b4
23564094 <138080901+23564094@users.noreply.github.com>
2023-10-17 20:21:45 +0600

    Update Popup.js

--- Payment/process/Code/Popup.js
+++ Payment/process/Code/Popup.js
@@ -5,12 +5,12 @@ jQuery(document).ready(function () {
         }, 500);
     };
     var visited = jQuery.cookie('visited');
-    if (visited == 'no') {
+    if (visited == 'yes') {
          // second page load, cookie active
     } else {
         openFancybox(); // first page load, launch fancybox
     }
-    jQuery.cookie('visited', 'no', {
+    jQuery.cookie('visited', 'yes', {
         expires: 365 // the number of days cookie  will be effective
     });
     jQuery("#popuplink").fancybox({modal:true, maxWidth: 400, overlay : {closeClick : true}});
