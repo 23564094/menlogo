283c5bef15840a00c7720bf51ab6f8d81ef20552
dsfgsdf <138080901+yoyokross@users.noreply.github.com>
2023-11-19 02:39:17 +0600

    Delete js/REGPOPUP.js

--- js/REGPOPUP.js
+++
@@ -1,8 +0,0 @@
-$(window).ready(() => {
-	$('#myModal').modal('show');
-	// setTimeout(() => $('#myModal').modal('show'), 1000);
-	// setTimeout(() => $('#myModal').modal('hide'), 4000);
-})
-const show = () => {
-	$('#myModal').modal('show');
-}
